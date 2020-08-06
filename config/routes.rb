dc0f26995271aca2459536ec6abd6c684fe04f34
Shuya Sawa <sawa.shuya.infratop@gmail.com>
2020-08-06 23:17:24 +0900

    start task1

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	scope module: :public do
+		devise_for :end_users
+  		# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  	end
+
+
+  	namespace :admin do
+  		devise_for :admins, path: ""
+  	end
 end
