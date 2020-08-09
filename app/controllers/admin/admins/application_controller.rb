b1f6b85f59d2e9a90e462116baacff69da24aaa1
Shuya Sawa <sawa.shuya.infratop@gmail.com>
2020-08-09 12:36:15 +0900

    making sign up key

--- app/controllers/admin/admins/application_controller.rb
+++ app/controllers/admin/admins/application_controller.rb
@@ -1,15 +1,12 @@
-class Public::EndUsers::ApplicationController < ApplicationController
+class Admin::Admins::ApplicationController < ApplicationController
 	before_action :configure_permitted_parameters, if: :devise_controller?
-	before_action :authenticate_user!
 
 	def after_sign_in_path_for(resource)
 			end_user_path(current_user.id)
-
 	end
 
 	 def configure_permitted_parameters
       		devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, 
       			:kana_last_name, :kana_first_name, :postal_code, :address, :telephone_number])
      end
-
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+	before_action :configure_permitted_parameters, if: :devise_controller?
+	before_action :authenticate_end_user!, except: [:top, :about]
 
 	def after_sign_in_path_for(resource)
 		if resource == :end_user
@@ -10,9 +12,12 @@ class ApplicationController < ActionController::Base
 
 	end
 
+	private
+
 	 def configure_permitted_parameters
 		 if resource == :end_user
-      		devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, ])
+      		devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, 
+      			:kana_last_name, :kana_first_name, :postal_code, :address, :telephone_number])
       	end
     end
 end
