a397e0f2d85556d6c7c9d544c245247eb571cb32
Shuya Sawa <sawa.shuya.infratop@gmail.com>
2020-08-16 12:35:59 +0900

    finish task4

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,62 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
 Admin.create!(
-	email: "a@a",
-	password: "aaaaaa"
+	email: "a@a"
+	encrypted_password: "aaaaaa"
+
+)
+
+
+Genre.create!(
+	[
+		{
+		id: 1,
+		name: "ケーキ",
+		is_active: true
+		},
+		{
+		id: 2,
+		name: "焼き菓子",
+		is_active: true
+		},
+		{
+		id: 3,
+		name: "キャンディ",
+		is_active: true
+		}
+	]
+
+)
+
+Item.create!(
+	[
+		{
+		genre_id: 1,
+		name: "チーズケーキ",
+		explanation: "自家製ミルクを使用した濃厚な味わい",
+		non_taxed_price: 2000,
+		item_image: File.open("/home/vagrant/work/NaganoCake/app/assets/images/cheese_cake.jpg"),
+		is_active: true
+		},
+		{
+		genre_id: 2,
+		name: "クッキー",
+		explanation: "口触りよし。お土産にもおすすめ",
+		non_taxed_price: 1500,
+		item_image: File.open("/home/vagrant/work/NaganoCake/app/assets/images/cookie.jpg"),
+		is_active: true
+		},
+		{
+		genre_id: 3,
+		name: "キャンディー",
+		explanation: "甘さと酸味のバランスが心地よい",
+		non_taxed_price: 1000,
+		item_image: File.open("/home/vagrant/work/NaganoCake/app/assets/images/candy.jpg"),
+		is_active: true
+		}
+	]
+
 )
