98d7e8fe45c9b790f43ac5ac2f04879a429a0084
Shuya Sawa <sawa.shuya.infratop@gmail.com>
2021-03-20 21:15:56 +0900

    fix_end_user

--- app/controllers/public/end_users_controller.rb
+++ app/controllers/public/end_users_controller.rb
@@ -1,15 +1,11 @@
 class Public::EndUsersController < ApplicationController
-  def show
-  	@my_data = EndUser.find(current_end_user.id)
-  end
+  before_action :set_end_user, except: [:unsubscribe]
+  def show; end
 
-  def edit
-  	@my_data = EndUser.find(current_end_user.id)
-  end
+  def edit; end
 
 
   def update
-  	@my_data = EndUser.find(current_end_user.id)
   	if @my_data.update(end_user_params)
   		flash[:notice] = "edit my data successfly"
   		redirect_to my_page_path
@@ -23,11 +19,10 @@ class Public::EndUsersController < ApplicationController
   end
 
   def withdrawal_process
-    @my_data = EndUser.find(current_end_user.id)
     if @my_data.update(is_unsubscribe: true)
-        reset_session
-        flash[:notice] = "unsubscribe succesfully"
-        redirect_to root_path
+      reset_session
+      flash[:notice] = "unsubscribe succesfully"
+      redirect_to root_path
     else
       flash[:notice] = "cannot unsubscribe"
       redirect_to my_page_path
@@ -39,6 +34,10 @@ class Public::EndUsersController < ApplicationController
   private
   def end_user_params
   	params.require(:end_user).permit(:last_name, :first_name, :kana_last_name, :kana_first_name, :postal_code, :address, :telephone_number, :email)
+  end
+
+  def set_end_user
+    @my_data = EndUser.find(current_end_user.id)
   end
 
 
