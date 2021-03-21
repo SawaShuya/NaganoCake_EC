beff25f89b618790140c73242774b11636bf8903
Shuya Sawa <sawa.shuya.infratop@gmail.com>
2021-03-21 17:51:19 +0900

    fix_end_user_address

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,34 +1,36 @@
 class Public::AddressesController < ApplicationController
-	def index
-		@address = Address.new
-		@addresses = Address.all
-	end
+	before_action :set_address, only: [:index, :edit]
+	before_action :set_addresses, only: [:index]
+	def index; end
 
-	def edit
-	end
+	def edit; end
 
 	def create
-		@address = Address.new(address_params)
-		@address.end_user_id = current_end_user.id
+		@address = current_end_user.addresses.new(address_params)
 		if @address.save
 			flash[:notice] = "create new address successflly"
 			redirect_to addresses_path
 		else
 			flash[:notice] = "cannot create new address"
-			@addresses = Address.all
+			set_addresses
 			render "index"
 		end
 
 	end
 
-	def update
-	end
+	def update; end
 
-	def destroy
-	end
+	def destroy; end
 
 	def address_params
 		params.require(:address).permit(:consignee, :postal_code, :destination)
 	end
 
+	def set_address
+		@address = Address.find_or_initialize_by(id: params[:id])
+	end
+
+	def set_addresses
+		@addresses = Address.where(end_user_id: current_end_user.id)
+	end
 end
